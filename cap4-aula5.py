9e2c49fff3513d9dd772f58907fffea7f5a43969
Douglas-SQ <douglashdcloud@gmail.com>
2023-06-22 18:13:49 -0300

    desafios

---
+++ cap4-aula5.py
@@ -0,0 +1,9 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(x=974,y=473,button='right',duration=2)
+pyautogui.click(x=1037,y=381,duration=1)
+pyautogui.moveTo(x=837,y=390,duration=1)
+pyautogui.click(x=830,y=390,button='right', duration=1)
+pyautogui.click(button='left')
+
---
+++ cap4-aula7-desafio-1.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+for i in range(5):
+    pyautogui.moveTo(878,196,duration=1)
+    pyautogui.dragTo(884,548,button='left',duration=1)
