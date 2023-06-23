6fcef10b50a398fdbd9554a15ded512e9b554d6e
Douglas-SQ <douglashdcloud@gmail.com>
2023-06-22 21:46:36 -0300

    aula11

--- cap4-aula11-desafio.py
+++ cap4-aula11-desafio.py
@@ -1,7 +1,16 @@
 import pyautogui
 import pyperclip
 
+pyautogui.alert(text='Iniciando automação',title='alerta')
 email=pyautogui.prompt(text='Informe o seu e-mail',title='Informações')
 senha=pyautogui.password(text='Informe a sua senha',title='senha',mask='*')
+pyautogui.click(922,263,duration=1)
 
 pyperclip.copy(email)
+pyautogui.hotkey('ctrl','v')
+pyautogui.press('enter')
+pyperclip.copy(senha)
+pyautogui.hotkey('ctrl','v')
+
+resposta=pyautogui.confirm(text='deseja continuar?',buttons=['sim','não'])
+pyautogui.alert(text=resposta,title='confirmação')
